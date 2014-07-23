48a6b3e65769b488c6bbbea8e302fe048eaa6748
Luke Murphy <lukewm@bitmessage.ch>
2014-07-23 11:52:29 +0100

    add gallery images + remove unused divider

--- orchard_theme/index.php
+++ orchard_theme/index.php
@@ -29,10 +29,9 @@
           <ul class="nav navbar-nav">
             <li><a href="#home" class="smoothScroll">Home</a></li>
             <li> <a href="#about" class="smoothScroll"> About</a></li>
-            <li> <a href="#services" class="smoothScroll"> Services</a></li>
+            <li> <a href="#services" class="smoothScroll"> Timetable</a></li>
             <li> <a href="#team" class="smoothScroll"> Team</a></li>
-            <li> <a href="#portfolio" class="smoothScroll"> Portfolio</a></li>
-            <li> <a href="#blog" class="smoothScroll"> Blog</a></li>
+            <li> <a href="#portfolio" class="smoothScroll"> Gallery</a></li>
             <li> <a href="#contact" class="smoothScroll"> Contact</a></li>
         </div><!--/.nav-collapse -->
       </div>
@@ -111,7 +110,7 @@
         <div class="container" id="team" name="team">
         <br>
             <div class="row white centered">
-                <h1 class="centered">MEET OUR TEAM</h1>
+                <h1 class="centered">MEET THE FOUNDERS</h1>
                 <hr>
                 <br>
                 <br>
@@ -169,7 +168,7 @@
         <br>
             <div class="row">
                 <br>
-                <h1 class="centered fancy-text">WE CREATE COOL STUFF</h1>
+                <h1 class="centered fancy-text">We create a safe and fun environment</h1>
                 <hr>
                 <br>
                 <br>
@@ -183,8 +182,7 @@
                         <figure>
                             <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio/folio01.jpg" alt="">
                             <figcaption>
-                                <h5>DASHBOARD</h5>
-                                <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+                                <h5>Pickaboo!</h5>
                             </figcaption><!-- /figcaption -->
                         </figure><!-- /figure -->
                     </div><!-- /grid-mask -->
@@ -220,8 +218,7 @@
                         <figure>
                             <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio/folio02.jpg" alt="">
                             <figcaption>
-                                <h5>UI DESIGN</h5>
-                                <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+                                <h5>Ball pen fun!</h5>
                             </figcaption><!-- /figcaption -->
                         </figure><!-- /figure -->
                     </div><!-- /grid-mask -->
@@ -233,56 +230,13 @@
                         <figure>
                             <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio/folio03.jpg" alt="">
                             <figcaption>
-                                <h5>ANDROID PAGE</h5>
-                                <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+                                <h5>Painting time!</h5>
                             </figcaption><!-- /figcaption -->
                         </figure><!-- /figure -->
                     </div><!-- /grid-mask -->
                 </div><!-- /col -->
             </div><!-- /row -->
 
-                <!-- PORTFOLIO IMAGE 4 -->
-            <div class="row">   
-                <div class="col-md-4 ">
-                    <div class="grid mask">
-                        <figure>
-                            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio/folio04.jpg" alt="">
-                            <figcaption>
-                                <h5>PROFILE</h5>
-                                <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
-                            </figcaption><!-- /figcaption -->
-                        </figure><!-- /figure -->
-                    </div><!-- /grid-mask -->
-                </div><!-- /col -->
-                
-                <!-- PORTFOLIO IMAGE 5 -->
-                <div class="col-md-4">
-                    <div class="grid mask">
-                        <figure>
-                            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio/folio05.jpg" alt="">
-                            <figcaption>
-                                <h5>TWITTER STATUS</h5>
-                                <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
-                            </figcaption><!-- /figcaption -->
-                        </figure><!-- /figure -->
-                    </div><!-- /grid-mask -->
-                </div><!-- /col -->
-                
-                <!-- PORTFOLIO IMAGE 6 -->
-                <div class="col-md-4">
-                    <div class="grid mask">
-                        <figure>
-                            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio/folio06.jpg" alt="">
-                            <figcaption>
-                                <h5>PHONE MOCKUP</h5>
-                                <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
-                            </figcaption><!-- /figcaption -->
-                        </figure><!-- /figure -->
-                    </div><!-- /grid-mask -->
-                </div><!-- /col -->
-            </div><!-- /row -->
-            <br>
-            <br>
         </div><!-- /row -->
     </div><!-- /container -->
 
@@ -290,63 +244,6 @@
         <section class="section-divider textdivider divider4">
             <div class="container">
                 <h1 class="fancy-text">HEADER #2</h1>
-                <hr>
-            </div><!-- container -->
-        </section><!-- section -->
-        
-        <!-- ==== BLOG ==== -->
-        <div class="container" id="blog" name="blog">
-        <br>
-            <div class="row">
-                <br>
-                <h1 class="centered">WE ARE STORYTELLERS</h1>
-                <hr>
-                <br>
-                <br>
-            </div><!-- /row -->
-            
-            <div class="row">
-                <div class="col-lg-6 blog-bg">
-                    <div class="col-lg-4 centered">
-                    <br>
-                        <p><img class="img img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/team/team04.jpg" width="60px" height="60px"></p>
-                        <h4>Jaye Smith</h4>
-                        <h5>Published Aug 30.</h5>
-                    </div>
-                    <div class="col-lg-8 blog-content">
-                        <h2>We Define Success</h2>
-                        <p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
-                        <p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
-                        <p><a href="#" class="icon icon-link"> Read More</a></p>
-                        <br>
-                    </div>
-                </div><!-- /col -->
-                
-                <div class="col-lg-6 blog-bg">
-                    <div class="col-lg-4 centered">
-                    <br>
-                        <p><img class="img img-circle" src="<?php echo get_template_directory_uri(); ?>/assets/img/team/team03.jpg" width="60px" height="60px"></p>
-                        <h4>Michele Lampa</h4>
-                        <h5>Published Aug 28.</h5>
-                    </div>
-                    <div class="col-lg-8 blog-content">
-                        <h2>A Beautiful Story</h2>
-                        <p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
-                        <p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
-                        <p><a href="#" class="icon icon-link"> Read More</a></p>
-                        <br>
-                    </div>
-                </div><!-- /col -->
-            </div><!-- /row -->
-            <br>
-            <br>
-        </div><!-- /container -->
-
-        
-        <!-- ==== SECTION DIVIDER6 ==== -->
-        <section class="section-divider textdivider divider6">
-            <div class="container">
-                <h1 class="fancy-text">Created in Cork, Ireland</h1>
                 <hr>
             </div><!-- container -->
         </section><!-- section -->
